3d9702db08a7dbbf5960c3b765b004a628a1d9d7
gaku3601 <pro.gaku@gmail.com>
2018-03-15 08:18:16 +0900

    circleciの設定

--- .circleci/product.deploy.sh
+++ .circleci/product.deploy.sh
@@ -1,5 +1,7 @@
 # ACCTからサービスアカウントキーファイルを作成
-echo $ACCT_AUTH | base64 -d > ${HOME}/account-auth.json
-cd ${HOME}
-pwd
-cat ./account-auth.json
+echo $ACCT_AUTH | base64 -d > ${HOME}/gcloud-service-key.json
+
+# gloudの設定
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
