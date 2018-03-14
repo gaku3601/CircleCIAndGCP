798ff6d84ae4012f80fa395c0f33d7556328ba91
gaku3601 <pro.gaku@gmail.com>
2018-03-15 08:37:47 +0900

    circleciの設定

--- .circleci/product.deploy.sh
+++ .circleci/product.deploy.sh
@@ -5,3 +5,7 @@ echo $ACCT_AUTH | base64 -d > ${HOME}/gcloud-service-key.json
 sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
 sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 sudo /opt/google-cloud-sdk/bin/gcloud config set project gyukame-197821
+
+# コンテナのUPLOAD
+docker build -t asia.gcr.io/gyukame-197821/hello:$CIRCLE_SHA1 .
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/gyukame-197821/hello:$CIRCLE_TAG
