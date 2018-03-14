e165db81d24800057dd8ed895802329a0f5c2895
gaku3601 <pro.gaku@gmail.com>
2018-03-15 08:14:58 +0900

    circleciの設定

--- .circleci/product.deploy.sh
+++ .circleci/product.deploy.sh
@@ -1,5 +1,5 @@
 # ACCTからサービスアカウントキーファイルを作成
-echo $ACCT_AUTH | base64 -D > ${HOME}/account-auth.json
+echo $ACCT_AUTH | base64 -d > ${HOME}/account-auth.json
 cd ${HOME}
 pwd
 cat ./account-auth.json
